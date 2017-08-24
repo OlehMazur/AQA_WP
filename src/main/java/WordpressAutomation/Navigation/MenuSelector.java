e57ffa31284ea1ae86f7d55a333c90b63de91e4a
Oleh <olegmazurbidev@gmail.com>
2017-08-24 18:38:29 +0300

    new test

--- src/main/java/WordpressAutomation/Navigation/MenuSelector.java
+++ src/main/java/WordpressAutomation/Navigation/MenuSelector.java
@@ -2,13 +2,20 @@ package WordpressAutomation.Navigation;
 
 import WordpressAutomation.Selenium.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by oleh on 22.08.17.
  */
 public class MenuSelector {
     public static void Select(String topLevelMenuId, String subMenuLinkText) {
+
         Driver.Instance.findElement(By.id(topLevelMenuId)).click();
+
+        WebDriverWait wait = new WebDriverWait(Driver.Instance, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText(subMenuLinkText)));
+
         Driver.Instance.findElement(By.linkText(subMenuLinkText)).click();
     }
 }
